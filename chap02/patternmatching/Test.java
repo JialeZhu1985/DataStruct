17559170ee9fc455e71fee3663db4e683b2745a4
panshanwhut <panshanwhut@163.com>
2016-11-16 16:56:04 +0800

    实现字符串匹配的KMP算法

--- chap02/patternmatching/Test.java
+++ chap02/patternmatching/Test.java
@@ -5,7 +5,15 @@ public class Test {
 		String target = "asdfghjk";
 		String pattern = "dfghjk";
 		
-		BruteForce bf = new BruteForce();
-		System.out.println(bf.indexOf(target, pattern, 0));
+//		BruteForce bf = new BruteForce();
+//		System.out.println(bf.indexOf(target, pattern, 0));
+		KMP kmp = new KMP();
+		pattern = "abcabdabcabcaa";
+//		int[] res = kmp.getNext(pattern);
+//		for(int item:res)
+//			System.out.println(item);
+		target = "acabbabbabc";
+		pattern = "abbabc";
+		System.out.println(kmp.indexOf(target, pattern));
 	}
 }
