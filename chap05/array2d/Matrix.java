90331001f5697a95e2d84e1df9bf749455a3a630
panshan <panshanwhut@163.com>
2016-11-21 09:50:58 +0800

    完善矩阵类的reSet()操作

--- chap05/array2d/Matrix.java
+++ chap05/array2d/Matrix.java
@@ -2,7 +2,7 @@ package array2d;
 
 /**
  * 基于二维数组的矩阵类
- * */
+ */
 public class Matrix {
 	private int rows, cols; // 矩阵的行列数
 	private int[][] elements; // 二维数组
@@ -23,43 +23,59 @@ public class Matrix {
 				this.elements[row][col] = values[row][col];
 			}
 		}
-		
+
 	}
-	
-	//返回矩阵行列数
-	public int getRows(){
+
+	// 返回矩阵行列数
+	public int getRows() {
 		return this.rows;
 	}
-	
-	public int getCols(){
+
+	public int getCols() {
 		return this.cols;
 	}
-	
-	//返回指定元素、设置指定元素值(对索引容错)
-	public int get(int row, int col){
-		if(0 <= row && row < this.rows && 0<= col && col < this.cols)
+
+	// 返回指定元素、设置指定元素值(对索引容错)
+	public int get(int row, int col) {
+		if (0 <= row && row < this.rows && 0 <= col && col < this.cols)
 			return this.elements[row][col];
 		else
-			throw new IndexOutOfBoundsException("row="+row+",col="+col);
+			throw new IndexOutOfBoundsException("row=" + row + ",col=" + col);
 	}
-	
-	public void set(int row, int col, int value){
-		if(0 <= row && row < this.rows && 0<= col && col < this.cols)
+
+	public void set(int row, int col, int value) {
+		if (0 <= row && row < this.rows && 0 <= col && col < this.cols)
 			this.elements[row][col] = value;
 		else
-			throw new IndexOutOfBoundsException("row="+row+",col="+col);
+			throw new IndexOutOfBoundsException("row=" + row + ",col=" + col);
 	}
-	
-	//重置矩阵行列值：若参数指定的行列大于当前行列，则扩容；若小于当前行列，则丢弃多余元素
-	public void reSet(int rows, int cols){
-		if(rows <0 || cols < 0)
-			throw new IllegalArgumentException("rows="+rows+",cols"+cols);
-		else{
+
+	// 重置矩阵行列值：若参数指定的行列大于当前行列，则扩容；若小于当前行列，则丢弃多余元素
+	public void reSet(int rows, int cols) {
+		if (rows < 0 || cols < 0)
+			throw new IllegalArgumentException("rows=" + rows + ",cols" + cols);
+		else {
 			int[][] tmp = new int[rows][cols];
-			for(int row = 0; row < rows && row < this.rows; row++)
-				for(int col = 0; col < cols && col <this.cols; col++)
+			for (int row = 0; row < rows && row < this.rows; row++)
+				for (int col = 0; col < cols && col < this.cols; col++)
 					tmp[row][col] = this.elements[row][col];
+			
+			this.elements = tmp;
+			this.rows = rows;
+			this.cols = cols;
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for(int row = 0; row < rows; row++){
+			for(int col = 0; col < cols; col++)
+				sb.append(this.elements[row][col]+" ");
+			sb.append("\n");
 		}
 		
+		return sb.toString();
 	}
 }
